0db0802325bb6036bf3fa43065b5a47d694ebd99
Igor <ichaves@live.com>
2022-11-10 22:16:43 +0900

    quiz studies

--- testes de estudo/script.js
+++ testes de estudo/script.js
@@ -1,33 +1,28 @@
-// SORT() -> Ordena os elementos do array 
+const btn = document.querySelector("button")
+const divA = document.querySelector(".a")
+const divB = document.querySelector(".b")
+const divC = document.querySelector(".c")
+const divD = document.querySelector(".d")
 
-// const names = ["fulano", "beltrano", "ciclano"]
-// console.log(names)
-// const orderedNames = names.sort()
-// console.log(orderedNames)
+const answers = ["YES", "NO"]
 
+btn.addEventListener("click", () => {
+  const input1 = document.querySelector('input[name="yes-no1"]:checked')
+  const input2 = document.querySelector('input[name="yes-no2"]:checked')
 
+  if (input1.value === "YES") {
+    divA.style.background = "green"
+    divB.style.background = "none"
+  } else {
+    divA.style.background = "none"
+    divB.style.background = "red"
+  }
 
-
-
-// const numbers = [2, 44, 93, 18, 54, 7, 23]
-// console.log(numbers)
-// const orderedNumbers = numbers.sort()
-// console.log(orderedNumbers)
-
-
-
-
-
-const family = [
-    { name: 'Lineu', score: 20 },
-    { name: 'Nenê', score: 10 },
-    { name: 'Tuco', score: 50 },
-    { name: 'Bebel', score: 30 },
-    { name: 'Agostinho', score: 70 }
-]
-
-
-// se o segundo score menos o primeiro score der resultado positivo; o item 2 vem antes do item 1
-// se o primeiro score menos o segundo score der resultado negativo; o item 2 vem antes do item 2
-// se ambos os valores subtraidos resultarem em 0, os itens do array nao seo nomeados
-family.sort((item1, item2) => item2.score - item1.score)
+  if (input2.value === "NO") {
+    divD.style.background = "green"
+    divC.style.background = "none"
+  } else {
+    divD.style.background = "none"
+    divC.style.background = "red"
+  }
+})
