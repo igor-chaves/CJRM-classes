ddb35d5ae21722ae2199318871681932c1d2e73b
Igor <ichaves@live.com>
2022-11-16 21:02:04 +0900

    my own todo list updated

---
+++ Todo/script2.js
@@ -0,0 +1,71 @@
+const findForm = document.querySelector(".findForm")
+const findBtn = document.querySelector(".findButton")
+const findInput = document.querySelector(".findInput")
+const ul = document.querySelector(".mainList")
+const addForm = document.querySelector(".addForm")
+
+// Faz busca no TODO list (via formulário + botão)
+findForm.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const searchedValue = e.target.findInputName.value.trim()
+
+    Array.from(ul.children)
+        // Retorna só as LIs que NÃO (!) contém o valor digitado pelo usuário
+        .filter(li => !li.textContent.toLowerCase().includes(searchedValue))
+        .forEach(li => {
+            li.classList.add("hidden")
+        })
+    
+    Array.from(ul.children)
+        // Retorna só as LIs que CONTÉM o valor digitado pelo usuário
+        .filter(li => li.textContent.toLowerCase().includes(searchedValue))
+        .forEach(li => {
+            li.classList.remove("hidden")
+        })
+
+    e.target.reset()
+})
+
+// Faz busca no TODO list (a busca é realidade em tempo real ao digitar)
+findInput.addEventListener("input", e => {
+    const searchedValue = e.target.value.toLowerCase().trim()
+
+    Array.from(ul.children)
+        // Retorna só as LIs que NÃO (!) contém o valor digitado pelo usuário
+        .filter(li => !li.textContent.toLowerCase().includes(searchedValue))
+        .forEach(li => {
+            li.classList.add("hidden")
+        })
+    
+    Array.from(ul.children)
+        // Retorna só as LIs que CONTÉM o valor digitado pelo usuário
+        .filter(li => li.textContent.toLowerCase().includes(searchedValue))
+        .forEach(li => {
+            li.classList.remove("hidden")
+        })
+})
+
+// Add LIs no TODO list (pega valor do text input via formulário + botão)
+addForm.addEventListener("submit", e => {
+    e.preventDefault()
+
+    // const inputValue = e.target.addForm.input.value.trim() --> pega o valor do input do form (ADDFORM)
+    const inputValue = e.target.addInputName.value.trim()
+    if (inputValue.length) { // se tiver algo no input add, senao, nao faz nada
+        ul.innerHTML += `
+        <li>${inputValue}
+            <img class="deleteIcon" src="./trash.svg"></img>
+        </li>`
+    }
+
+    e.target.reset()
+})
+
+// Deleta LIs do TODO list (remove LIs da UL)
+ul.addEventListener("click", e => {
+    // e.target.remove()
+    if (Array.from(e.target.classList).includes("deleteIcon")) {
+        e.target.parentElement.remove()
+    }
+})
