a97368036c6dd8b94bd6b24b910e07a6e3649f63
Igor <ichaves@live.com>
2022-11-21 23:29:08 +0900

    Refactoring

--- Quiz/script3.js
+++ Quiz/script3.js
@@ -3,27 +3,44 @@ const formQuiz = document.querySelector(".container-quiz")
 const scoreDiv = document.querySelector(".container-score")
 const checkBtn = document.querySelector(".container-checkBtn")
 
+let score = 0
 
-checkBtn.addEventListener("click", e => {
-    e.preventDefault()
-
-    const userAnswers = [
+const getUserAnswers = () => {
+    return userAnswers = [
         formQuiz.question1.value,
         formQuiz.question2.value,
         formQuiz.question3.value,
     ]
+}
 
-    formQuiz.reset()
-
-    let score = 0
+const setScore = () => {
     quizAnswers.forEach((answer, i) => {
-        if (answer === userAnswers[i]) {
+        const answersMatches = answer === userAnswers[i]
+
+        if (answersMatches) {
             console.log("acertou a questao", i+1)
-            score += 33
+            return score += 33
         }
     })
-    console.log("Seu score:", score)
-    
+}
+
+const showScore = () => {
     scoreDiv.textContent = score
     scoreDiv.style.display = "flex"
-})
+}
+
+const resetFormAndScore = () => {
+    formQuiz.reset()
+    return score = 0
+}
+
+const checkAnswers = e => {
+    e.preventDefault()
+
+    getUserAnswers()
+    setScore(score)
+    showScore()
+    resetFormAndScore()
+}
+
+checkBtn.addEventListener("click", checkAnswers)
