fedede8205fdb912a0bc0b8bc526be6a302dd20d
Igor <ichaves@live.com>
2022-11-26 23:30:20 +0900

    added new files

--- testes de estudo/script.js
+++ testes de estudo/script.js
@@ -1,16 +1,77 @@
-const present = new Date()
+// const present = new Date()
 
-console.log(present)
-console.log(present.getFullYear())
-console.log(present.getMonth())
-console.log(present.getDate())
+// console.log(present)
+// console.log(present.getFullYear())
+// console.log(present.getMonth())
+// console.log(present.getDate())
 
-console.log(present.getHours())
-console.log(present.getMinutes())
-console.log(present.getSeconds())
+// console.log(present.getHours())
+// console.log(present.getMinutes())
+// console.log(present.getSeconds())
 
-console.log(present.getTime())
+// console.log(present.getTime())
 
-console.log(present.toDateString())
-console.log(present.toTimeString())
-console.log(present.toLocaleString())
+// console.log(present.toDateString())
+// console.log(present.toTimeString())
+// console.log(present.toLocaleString())
+
+
+
+
+
+//-----------------------------------------------------------------
+
+
+
+
+
+
+// const clockContainer = document.querySelector(".clock-container")
+
+// const updateClock = () => {
+//     const present = new Date()
+//     const hours = present.getHours()
+//     const minutes = present.getMinutes()
+//     const seconds = present.getSeconds()
+
+//     // o ternário verifica se tem 1 digito, se tiver adiciona o 0 antes pra ficar com 2 digitos
+//     const clockHTML = `
+//     <span>${String(hours).length === 1 ? `0${hours}` : hours}</span> :
+//     <span>${String(minutes).length === 1 ? `0${minutes}` : minutes}</span> :
+//     <span>${String(seconds).length === 1 ? `0${seconds}` : seconds}</span>
+//     `
+//     clockContainer.innerHTML = clockHTML
+// }
+
+// setInterval(updateClock, 1000)
+
+
+
+
+
+//-----------------------------------------------------------------
+
+
+
+
+
+// const present = new Date()
+// const past = new Date("Nov 24 2022 16:22:30")
+
+// console.log(dateFns.format(present, "MMMM"))
+// console.log(dateFns.format(present, "YY"))
+// console.log(dateFns.format(present, "dddd"))
+// console.log(dateFns.format(present, "Do"))
+// console.log(dateFns.format(present, "DD/MM/YYYY"))
+
+// console.log(dateFns.distanceInWords(present, past, { addSuffix: true }))
+
+
+
+
+
+//-----------------------------------------------------------------
+
+
+
+
