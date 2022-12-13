50f0c47a09cf789e3f6a18b50bb8153ddc41aeb6
Igor <ichaves@live.com>
2022-12-13 21:53:18 +0900

    Update app5.js

--- testes de estudo/app5.js
+++ testes de estudo/app5.js
@@ -1,7 +1,15 @@
-const request = new XMLHttpRequest()
-request.addEventListener("readystatechange", () => {
-    if (request.readyState === 4 && request.status === 200) console.log(request, request.responseText)
-})
+const getTodos = () => {
+    const request = new XMLHttpRequest()
 
-request.open("get", "https://jsonplaceholder.typicode.com/todos")
-request.send()
+    request.addEventListener("readystatechange", () => {
+        const requestOk = request.readyState === 4 && request.status === 200
+        const requestNotOk = request.readyState === 4
+        
+        if (requestOk) {console.log(request, request.responseText); return}
+        if (requestNotOk) console.log("Não foi possível obter os dados da API")
+    })
+    
+    request.open("get", "https://jsonplaceholder.typicode.com/todos")
+    request.send()
+}
+getTodos()
