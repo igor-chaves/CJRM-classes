4cb49dfd3e20f308dccef346f303800a00e92b53
Igor <ichaves@live.com>
2022-12-15 16:56:35 +0900

    Update app5.js

--- testes de estudo/app5.js
+++ testes de estudo/app5.js
@@ -1,25 +1,15 @@
-// const getTodos = () => {
-//     const request = new XMLHttpRequest()
+const getTodos = () => {
+    const request = new XMLHttpRequest()
 
-//     request.addEventListener("readystatechange", () => {
-//         const requestOk = request.readyState === 4 && request.status === 200
-//         const requestNotOk = request.readyState === 4
+    request.addEventListener("readystatechange", () => {
+        const requestOk = request.readyState === 4 && request.status === 200
+        const requestNotOk = request.readyState === 4
         
-//         if (requestOk) {console.log(request, request.responseText); return}
-//         if (requestNotOk) console.log("Não foi possível obter os dados da API")
-//     })
+        if (requestOk) {console.log(request, request.responseText); return}
+        if (requestNotOk) console.log("Não foi possível obter os dados da API")
+    })
     
-//     request.open("get", "https://jsonplaceholder.typicode.com/todos")
-//     request.send()
-// }
-// getTodos()
-
-
-const request = new XMLHttpRequest()
-
-
-if(request.readyState === 4) console.log("nao aprovado")
-if(request.readyState === 4 && request.status === 200) console.log("aprovado")
-request.open("get", "https://pokeapi.co/api/v2/pokemon/ditto")
-request.send()
-
+    request.open("get", "https://jsonplaceholder.typicode.com/todos")
+    request.send()
+}
+getTodos()
