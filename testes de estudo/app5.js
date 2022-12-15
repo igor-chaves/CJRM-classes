9379d987b05ff74bb2d7a8bfaa4049db46248316
Igor <ichaves@live.com>
2022-12-15 18:21:51 +0900

    Update app5.js

--- testes de estudo/app5.js
+++ testes de estudo/app5.js
@@ -1,16 +1,46 @@
-const getTodos = () => {
+// const getTodos = () => {
+//     const request = new XMLHttpRequest()
+
+//     request.addEventListener("readystatechange", () => {
+//         const requestOk = request.readyState === 4 && request.status === 200
+//         const requestNotOk = request.readyState === 4
+        
+//         if (requestOk) {console.log(request.responseText); return}
+//         if (requestNotOk) console.log("Não foi possível obter os dados da API")
+//     })
+    
+//     request.open("get", "https://jsonplaceholder.typicode.com/todos")
+//     request.send()
+// }
+
+// getTodos()
+
+
+
+// colocando uma funcao de callback na getTodos
+
+const getTodos = callback => {
     const request = new XMLHttpRequest()
 
     request.addEventListener("readystatechange", () => {
         const requestOk = request.readyState === 4 && request.status === 200
         const requestNotOk = request.readyState === 4
         
-        if (requestOk) {console.log(request.responseText); return}
-        if (requestNotOk) console.log("Não foi possível obter os dados da API")
+        if (requestOk) {callback(null, request.responseText); return} // (ERROR = null pq nao precisa, DATA)
+        if (requestNotOk) callback("Não foi possível obter os dados da API", null) // (mensagem, DATA = null pq nao precisa)
     })
     
     request.open("get", "https://jsonplaceholder.typicode.com/todos")
     request.send()
 }
 
-getTodos()
+console.log(1)
+console.log(2)
+
+getTodos((error, data) => {
+    if (error) {console.log(error); return}
+    console.log(data)
+})
+
+console.log(3)
+console.log(4)
