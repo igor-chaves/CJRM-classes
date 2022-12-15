d53f27e296ac4475ba19bf6211eea0ec8a9e9947
Igor <ichaves@live.com>
2022-12-15 17:57:20 +0900

    Update app5.js

--- testes de estudo/app5.js
+++ testes de estudo/app5.js
@@ -5,11 +5,12 @@ const getTodos = () => {
         const requestOk = request.readyState === 4 && request.status === 200
         const requestNotOk = request.readyState === 4
         
-        if (requestOk) {console.log(request, request.responseText); return}
+        if (requestOk) {console.log(request.responseText); return}
         if (requestNotOk) console.log("Não foi possível obter os dados da API")
     })
     
     request.open("get", "https://jsonplaceholder.typicode.com/todos")
     request.send()
 }
+
 getTodos()
