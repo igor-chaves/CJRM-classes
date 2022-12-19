ec3138fe08aafbf8dba5adc6cd0cbe22814332b7
Igor <ichaves@live.com>
2022-12-19 22:11:33 +0900

    Update app.js

--- Exercise-29/app.js
+++ Exercise-29/app.js
@@ -239,12 +239,18 @@ colors.forEach(logColorMessage)
 
     Dica: pesquise por Computed Property Names.
 */
-  
-  const people = [
+
+const people = [
     { id: 5 , name: 'Angelica', age: 18, federativeUnit: 'Pernambuco' },
     { id: 81, name: 'Thales', age: 19, federativeUnit: 'São Paulo' },
     { id: 47, name: 'Ana Carolina', age: 18, federativeUnit: 'Alagoas' },
     { id: 87, name: 'Felipe', age: 18, federativeUnit: 'Minas Gerais' },
     { id: 9 , name: 'Gabriel', age: 20, federativeUnit: 'São Paulo' },
     { id: 73, name: 'Aline', age: 19, federativeUnit: 'Brasília' }
-  ]
+]
+const createOrIncrementAgeFrequency = (acc, { age }) => {
+    acc[age] = acc[age] + 1 || 1
+    return acc
+}
+const agesFrequency = people.reduce(createOrIncrementAgeFrequency, {})
+console.log(agesFrequency)
