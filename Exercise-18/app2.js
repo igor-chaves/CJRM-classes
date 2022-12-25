628305f81f046f646e301d4957e6ab04fdca3e09
Igor <ichaves@live.com>
2022-12-25 20:38:03 +0900

    new file, bug fixed

---
+++ Exercise-18/app2.js
@@ -0,0 +1,42 @@
+const form = document.querySelector("#register-form")
+const input = document.querySelector("#username")
+const button = document.querySelector("#clear")
+const pSubmit = document.createElement("p")
+const pInput = document.createElement("p")
+
+const regex = /^[a-zA-Z0-9]{4,8}$/
+
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const usernameValue = input.value
+    const verifyUsername = regex.test(usernameValue)
+
+    if (verifyUsername) {
+        pSubmit.textContent = "valid username"
+        pSubmit.setAttribute("class", "submit-valid")
+        button.insertAdjacentElement("afterend", pSubmit)
+        return
+    }
+    pSubmit.textContent = "invalid username"
+    pSubmit.setAttribute("class", "submit-invalid")
+    button.insertAdjacentElement("afterend", pSubmit)
+})
+
+input.addEventListener("input", e => {
+    e.preventDefault()
+
+    const usernameValue = input.value
+    const verifyUsername = regex.test(usernameValue)
+
+    if (verifyUsername) {
+        pInput.textContent = "Valid username"
+        pInput.setAttribute("class", "username-valid")
+        input.insertAdjacentElement("afterend", pInput)
+        return
+    }
+    pInput.textContent = "Invalid username"
+    pInput.setAttribute("class", "username-invalid")
+    input.insertAdjacentElement("afterend", pInput)
+})
