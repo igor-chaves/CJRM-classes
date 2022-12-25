ea011dd3e39b3ae0747e28d99e5fff2b15f1d166
Igor <ichaves@live.com>
2022-12-25 21:26:33 +0900

    Update app2.js

--- Exercise-18/app2.js
+++ Exercise-18/app2.js
@@ -6,7 +6,6 @@ const pInput = document.createElement("p")
 
 const regex = /^[a-zA-Z0-9]{4,8}$/
 
-
 form.addEventListener("submit", e => {
     e.preventDefault()
 
@@ -14,12 +13,12 @@ form.addEventListener("submit", e => {
     const verifyUsername = regex.test(usernameValue)
 
     if (verifyUsername) {
-        pSubmit.textContent = "valid username"
+        pSubmit.textContent = "User registered"
         pSubmit.setAttribute("class", "submit-valid")
         button.insertAdjacentElement("afterend", pSubmit)
         return
     }
-    pSubmit.textContent = "invalid username"
+    pSubmit.textContent = "Please insert a valid username"
     pSubmit.setAttribute("class", "submit-invalid")
     button.insertAdjacentElement("afterend", pSubmit)
 })
@@ -30,6 +29,8 @@ input.addEventListener("input", e => {
     const usernameValue = input.value
     const verifyUsername = regex.test(usernameValue)
 
+    pSubmit.remove()
+
     if (verifyUsername) {
         pInput.textContent = "Valid username"
         pInput.setAttribute("class", "username-valid")
@@ -39,4 +40,12 @@ input.addEventListener("input", e => {
     pInput.textContent = "Invalid username"
     pInput.setAttribute("class", "username-invalid")
     input.insertAdjacentElement("afterend", pInput)
+})
+
+button.addEventListener("click", e => {
+    e.preventDefault()
+    
+    input.value = ""
+    pInput.remove()
+    pSubmit.remove()
 })
