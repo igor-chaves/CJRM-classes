ed349e2c14a6aca13899542fe3f4e8b77fc504c1
Igor <ichaves@live.com>
2023-01-02 02:23:09 +0900

    Update app.js

--- Exercise-29/app.js
+++ Exercise-29/app.js
@@ -1,5 +1,5 @@
 // ESTUDANDO HTTP REQUESTS
-/*
+
 // const getTodos = () => {
 //     const request = new XMLHttpRequest()
 
@@ -15,67 +15,33 @@
 //     request.send()
 // }
 // getTodos()
-*/
+
 
 // ----------------------------------------------------------------------------------------------------------------------- //
 
 // ADICIONANDO FUNCÃO DE CALLBACK NA GETTODOS
-/*
-// const getTodos = callback => {
-//     const request = new XMLHttpRequest()
 
-//     request.addEventListener("readystatechange", () => {
-//         const requestOk = request.readyState === 4 && request.status === 200
-//         const requestNotOk = request.readyState === 4
-        
-//         if (requestOk) {callback(null, request.responseText); return} // (ERROR = null pq nao precisa, DATA)
-//         if (requestNotOk) callback("Não foi possível obter os dados da API", null) // (mensagem, DATA = null pq nao precisa)
-//     })
-    
-//     request.open("get", "https://jsonplaceholder.typicode.com/todos")
-//     request.send()
-// }
+const getTodos = callback => {
+    const request = new XMLHttpRequest()
 
-// getTodos((error, data) => {
-//     if (error) {console.log(error); return}
-//     console.log(data)
-// })
-*/
+     request.addEventListener("readystatechange", () => {
+        const requestOk = request.readyState === 4 && request.status === 200
+        const requestNotOk = request.readyState === 4
 
-// ----------------------------------------------------------------------------------------------------------------------- //
+        if (requestOk) {callback(null, request.responseText); return} // (ERROR = null pq nao precisa, DATA)
+        if (requestNotOk) callback("Não foi possível obter os dados da API", null) // (mensagem, DATA = null pq nao precisa)
+    })
 
-// ESTUDANDO JSON
-/*
-// const getTodos = (url, callback) => {
-//     const request = new XMLHttpRequest()
+    request.open("get", "https://jsonplaceholder.typicode.com/todos")
+    request.send()
+}
 
-//     request.addEventListener("readystatechange", () => {
-//         const requestOk = request.readyState === 4 && request.status === 200
-//         const requestNotOk = request.readyState === 4
-        
-//         if (requestOk) {
-//             const data = JSON.parse(request.responseText)
-//             callback(null, data); return
-//         } // (ERROR = null pq nao precisa, DATA)
-//         if (requestNotOk) callback("Não foi possível obter os dados da API", null) // (mensagem, DATA = null pq nao precisa)
-//     })
-    
-//     request.open("get", url)
-//     // request.open("get", "https://jsonplaceholder.typicode.com/todos")
-//     // request.open("get", "https://pokeapi.co/api/v2/pokemon/pikachu")
-//     request.send()
-// }
+const getErrorOrDataMessage = (error, data) => {
+    if (error) {console.log(error); return}
+    console.log(data)
+}
 
-// getTodos("./JSON/app5.json", (error, data) => {
-//     console.log(data)
-//     getTodos("./JSON/app5-02.json", (error, data) => {
-//         console.log(data)
-//         getTodos("./JSON/app5-03.json", (error, data) => {
-//             console.log(data)
-//         })
-//     })
-// })
-*/
+getTodos(getErrorOrDataMessage)
 
 
 
