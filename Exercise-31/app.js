7f4464f5a04888c7dace7f9fc027fcdc583c7e21
Igor <ichaves@live.com>
2023-01-04 21:10:36 +0900

    Update app.js

--- Exercise-31/app.js
+++ Exercise-31/app.js
@@ -1,7 +1,23 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => {
-        console.log("RESPONSE:", response)
-        return response.json()
-    })
-    .then(users => console.log(users))
-    .catch(error => console.log("ERROR:", error)) // só executa quando dá erro de conexão na rede
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(response => {
+//         console.log("RESPONSE:", response)
+//         return response.json()
+//     })
+//     .then(users => console.log(users))
+//     .catch(error => console.log("ERROR:", error)) // só executa quando dá erro de conexão na rede
+
+
+const getUsers = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    return await response.json()
+}
+const logUsers = async () => {
+    const users = await getUsers()
+    console.log(users)
+}
+
+console.log(1)
+console.log(2)
+logUsers()
+console.log(3)
+console.log(4)
