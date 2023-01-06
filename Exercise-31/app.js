f66e352cc9e6477f96fe29d88618d4bcb090c477
Igor <ichaves@live.com>
2023-01-06 21:57:58 +0900

    Update app.js

--- Exercise-31/app.js
+++ Exercise-31/app.js
@@ -5,11 +5,15 @@
     - Implemente uma segunda função que exibe, no console, seus dados de usuário 
       do GitHub.
 */
-// fetch("https://github.com/igor-chaves")
-    // .then(console.log("OK IGOR"))
-    // .catch(console.log("ERROR..."))
+const getGithubUser = async username => {
+    const response = await fetch(`https://api.github.com/users/${username}`)
+    return response.json()
+}
 
+const logGithubUser = async username =>
+    console.log(await getGithubUser(username))
 
+logGithubUser("igor-chaves")
 
 
 
@@ -19,9 +23,10 @@
     - Exiba esse novo array no console.
 */
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const newNum = numbers.filter(num => num % 2 === 0 || num % 3 === 0)
-console.log(numbers)
-console.log(newNum)
+const getDivisibleBy2Or3 = array => array
+    .filter(num => num % 2 === 0 || num % 3 === 0)
+console.log(getDivisibleBy2Or3(numbers))
+
 
 
 
@@ -63,57 +68,81 @@ console.log(newName)
 
     Dica: pesquise pelo método split.
 */
+// const igor = "igor"
+// const igorArray = igor.split("")
+// console.log(igorArray)
 const name = Array.from("Igor")
 name.map((letter, index) => console.log(`${letter} é a ${index + 1}ª letra do meu nome.`))
 
-/*
-05
 
-- Crie um objeto com as seguintes propriedades e tipos de valores:
-- name: String
-- lastname: String
-- age: Number
-- Exiba no console, em um array, todas as propriedades do objeto acima;
-- Não use nenhuma estrutura de repetição, nem crie o array manualmente.
 
-Dica: pesquise pelo método Object.keys().
+
+
+/* 05
+    - Crie um objeto com as seguintes propriedades e tipos de valores:
+    - name: String
+    - lastname: String
+    - age: Number
+    - Exiba no console, em um array, todas as propriedades do objeto acima;
+    - Não use nenhuma estrutura de repetição, nem crie o array manualmente.
+
+    Dica: pesquise pelo método Object.keys().
 */
+const obj = {
+    name: "igor",
+    lastname: "donega",
+    age: 33,
+}
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
 
-/*
-06
 
-- Implemente uma função que retorna as ocorrências de um determinado valor em 
-um array;
-- A função deve receber um array como 1º argumento e o valor a ser buscado no 
-array como 2º argumento;
-- Exemplo: se na invocação da função o array [1, 2, 3, 1] é passado como 1º 
-argumento e o número 1 é passado como 2º argumento, a função deve retornar 
-2, pois há duas ocorrências do número 1 no array;
-- Utilize o array abaixo para testar a função.
+
+
+
+/* 06
+    - Implemente uma função que retorna as ocorrências de um determinado valor em 
+      um array;
+    - A função deve receber um array como 1º argumento e o valor a ser buscado no 
+      array como 2º argumento;
+    - Exemplo: se na invocação da função o array [1, 2, 3, 1] é passado como 1º 
+      argumento e o número 1 é passado como 2º argumento, a função deve retornar 
+      2, pois há duas ocorrências do número 1 no array;
+    - Utilize o array abaixo para testar a função.
 */
 
 const scores = [100, 90, 85, 100, 60, 85, 100, 90, 55, 75, 60]
 
-/*
-07
+const findItem = (array, item) => {
+    let count = 0
 
-- Já implementamos o método some e o método map, do zero;
-- Neste exercício, seu desafio será criar o método filter, do zero;
-- Implemente uma função "filter" que possui a mesma funcionalidade do método  
-filter original;
-- Você não poderá utilizar o método filter original, embutido na linguagem;
-- A assinatura e retorno da invocação desta função devem ser os seguintes:
-- filter([1, 2, 3], item => item) // [1, 2, 3];
-- filter([0, 1, 2], item => item) // [1, 2];
-- filter([1, 2, 3], item => item < 2) // [1];
-- filter([1, 2, 3, 5], (item, index) => item === index + 1) // [1, 2, 3];
-- filter([1, 2, 3, 2, 1, 5], (item, index, array) =>
-    index === array.indexOf(item)) // [1, 2, 3, 5];
-- Utilize os casos acima para testar sua função;
-- Se você não se lembra como o método filter funciona, há 2 opções:
-1) Reassistir à aula "O método filter" - Aula 01-03 da etapa 08;
-2) Pesquisar no MDN.
+    array.forEach(num => {if(num === item ) count++})
+    console.log(count)
+}
+findItem(scores, 100)
 
-Dica: lembre-se que o método filter inclui o item em questão no novo array 
-que está sendo gerado **apenas** se a função retorna um valor truthy.
+
+
+
+
+/* 07
+    - Já implementamos o método some e o método map, do zero;
+    - Neste exercício, seu desafio será criar o método filter, do zero;
+    - Implemente uma função "filter" que possui a mesma funcionalidade do método  
+      filter original;
+    - Você não poderá utilizar o método filter original, embutido na linguagem;
+    - A assinatura e retorno da invocação desta função devem ser os seguintes:
+    - filter([1, 2, 3], item => item) // [1, 2, 3];
+    - filter([0, 1, 2], item => item) // [1, 2];
+    - filter([1, 2, 3], item => item < 2) // [1];
+    - filter([1, 2, 3, 5], (item, index) => item === index + 1) // [1, 2, 3];
+    - filter([1, 2, 3, 2, 1, 5], (item, index, array) =>
+      index === array.indexOf(item)) // [1, 2, 3, 5];
+    - Utilize os casos acima para testar sua função;
+    - Se você não se lembra como o método filter funciona, há 2 opções:
+    1) Reassistir à aula "O método filter" - Aula 01-03 da etapa 08;
+    2) Pesquisar no MDN.
+
+    Dica: lembre-se que o método filter inclui o item em questão no novo array 
+    que está sendo gerado **apenas** se a função retorna um valor truthy.
 */
