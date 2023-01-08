5a6ef756b96a0acf810904c6865758064cb87dfd
Igor <ichaves@live.com>
2023-01-08 19:50:42 +0900

    exercise from teacher done

--- Exercise-32/app.js
+++ Exercise-32/app.js
@@ -11,19 +11,54 @@
     　　listados na documentação: https://developers.giphy.com/docs/api/endpoint#search
     - Ignore os avisos no console. Para limpá-lo, pressione "ctrl + L".
 */
-
 const form = document.querySelector("form")
+const GIFsContainer = document.querySelector("div")
 
+const APIKey = "EbQJ4Iy764uvo9YMBCjX8bPwxlyAsmoj"
+
+const getGIFUrl = GIFName => `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&limit=1&q=${GIFName}`
+
+const generateGIFImage = (downsizedGifUrl, downsizedGifTitle) => {
+    const img = document.createElement("img")
+
+    img.setAttribute("src", downsizedGifUrl)
+    img.setAttribute("alt", downsizedGifTitle)
+    
+    return img
+}
+
+const fetchGIF = async inputValue => {
+    try {
+        const GIFUrl = getGIFUrl(inputValue)
+        const response = await fetch(GIFUrl)
+    
+        if (!response.ok) {
+            throw new Error("Não foi possível obter os dados!")
+        }
+        return response.json()
+    } catch (error) {
+        alert(`Erro: ${error.message}`)
+    }
+}
+
+const insertGIFIntoDOM = async inputValue => {
+    const GIFData = await fetchGIF(inputValue)
+
+    if (GIFData) {
+        const downsizedGifUrl = GIFData.data[0].images.downsized.url
+        const downsizedGifTitle = GIFData.data[0].title
+        const img = generateGIFImage(downsizedGifUrl, downsizedGifTitle)
+        
+        GIFsContainer.insertAdjacentElement("afterbegin", img)
+    
+        form.reset()
+    }
+}
+    
 form.addEventListener("submit", e => {
     e.preventDefault()
 
-    let inputValue = e.target.search.value
+    const inputValue = e.target.search.value
 
+    insertGIFIntoDOM(inputValue)
 })
-
-const url = async (inputValue) => {
-    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=EbQJ4Iy764uvo9YMBCjX8bPwxlyAsmoj&limit=1&q=${inputValue}`)
-    return await response.json()
-}
-
-console.log(url())
